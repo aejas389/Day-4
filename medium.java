b337a987250c94051cc5a03d7cb1bc0474d2aeaf
aejas389 <23hr1a3252@mtieat.org>
2024-12-05 10:44:41 +0530

    Add files via upload

---
+++ medium.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class medium{
+
+    public static void main(String[] args){
+
+
+
+    Scanner sc = new Scanner(System.in);
+
+
+
+    int a=sc.nextInt();
+    int flag=0;
+    if((a>=10))
+        flag=1;
+    switch(flag){
+        case 0:
+            System.out.println("Not Eligible");
+            break;
+        case 1:
+            System.out.println("Not Eligible");
+
+        default:
+            break;
+
+    }
+
+
+       
+
+    }
+
+}
+
+
+    
+        
+        
