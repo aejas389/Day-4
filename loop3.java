c9f4eb1d3f8325cbac18880a50292efbd042ebba
aejas389 <23hr1a3252@mtieat.org>
2024-12-05 14:19:47 +0530

    Add files via upload

---
+++ loop3.java
@@ -0,0 +1,21 @@
+public class loop3 {
+    
+    
+    public static void main(String[] args)
+    {
+
+
+        int i=10;
+
+
+        
+        for(i=0;i*i<=1;i++)
+        {
+            System.out.println(i);
+            }
+        }
+    }
+    
+    
+    
+
